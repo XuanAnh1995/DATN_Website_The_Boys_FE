53f336ec0ac3debe3199a0a4bc5183ec9a880617
Lý Tính Nhiệm <136042948+rdagol@users.noreply.github.com>
2025-04-13 10:08:05 +0700

    Update cart logic code and login logic code

--- src/layout/AdminLayout/Header.jsx
+++ src/layout/AdminLayout/Header.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import { FaUserCircle, FaSignOutAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../store/userSlice";
 
 function Header() {
   const [showMenu, setShowMenu] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+    // Lấy trạng thái user từ Redux
+    const { isLoggedIn, name } = useSelector((state) => state.user);
+
+    const handleLogout = () => {
+      dispatch(logout());
+      navigate("/login");
+    };
 
   return (
 <div className="bg-white text-dark flex justify-between items-center px-6 py-3 shadow-md">
@@ -23,7 +36,7 @@ function Header() {
             <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
               Hồ sơ cá nhân
             </button>
-            <button className="block w-full text-left px-4 py-2 hover:bg-gray-100 flex items-center gap-2">
+            <button className="block w-full text-left px-4 py-2 hover:bg-gray-100 flex items-center gap-2" onClick={handleLogout} >
               <FaSignOutAlt /> Đăng xuất
             </button>
           </div>
